e8bc37dda0a531f2ac44fc49dbc1742e2aca63c9
Sue Anne Teo <sueanneteo@gmail.com>
2013-10-06 16:52:49 +0200

    Initial Commit of mooc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mooc::Application.config.secret_token = '990acd5e0dfa6fd277cde30938710935781e0e9c885fadf575f6f513f2c0970b7642950836b0ff94e8ddb3afe3e11453ce34af3a5590e22ed6ea6f541dc2853f'
