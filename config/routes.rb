11216c6844984f65cc2d79fd771aa78481cab98f
Sue Anne Teo <sueanneteo@gmail.com>
2013-10-06 22:34:40 +0200

    Add PAges

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Mooc::Application.routes.draw do
+  root :to => 'pages#home'
+
+  get 'about' => 'pages#about'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
